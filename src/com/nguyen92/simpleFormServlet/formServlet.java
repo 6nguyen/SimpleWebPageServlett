001513dc8548b79d915e45928dfb14ad47ac8daa
George Nguyen <g.nguyen92@yahoo.com>
2017-07-06 14:25:17 -0700

    Create form.html and formServlet.java skeleton

---
+++ src/com/nguyen92/simpleFormServlet/formServlet.java
@@ -0,0 +1,55 @@
+package com.nguyen92.simpleFormServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class formServlet
+ */
+@WebServlet("/formServlet")
+public class formServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public formServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Step 1:  set content type
+		response.setContentType("text/html");
+		
+		// Step 2:  get print writer
+		PrintWriter out = response.getWriter();
+		
+		// Step 3: generate HTML code
+		out.println("<html><body>");
+		out.println("");
+		out.println("");
+		out.println("");
+		out.println("");
+		out.println("</body></html>");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
